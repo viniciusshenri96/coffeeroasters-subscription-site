78cf440be9ad5cf5f2797408f3b3bc6c9e619645
Vinicius <vinicius_henrique96@hotmail.com>
2022-11-19 17:46:58 -0300

    :construction: Starting Features with JavaScript

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,9 @@ const button = document.querySelector(".header__menu-ham");
 // const menu = document.querySelector(".menu");
 // const close = document.querySelector(".close");
 const nav = document.querySelector(".header__list");
+const selectionContainer = document.querySelector(".plan__selection");
+const planTitle = document.querySelectorAll(".plan__title");
+const planContent = document.querySelectorAll(".plan__content");
 
 const openMenu = function () {
   // menu.classList.toggle("active");
@@ -12,3 +15,19 @@ const openMenu = function () {
 };
 
 button.addEventListener("click", openMenu);
+
+selectionContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("plan__title")) {
+    e.target.classList.toggle("active");
+    e.target.nextElementSibling.classList.toggle("active");
+  }
+
+  if (e.target.classList.contains("plan__reply")) {
+    const title = e.target.querySelector(".plan__reply--title");
+    const desc = e.target.querySelector(".plan__reply--desc");
+
+    e.target.style.backgroundColor = "#0e8784";
+    title.style.color = "#FFF";
+    desc.style.color = "#fefcf7";
+  }
+});
