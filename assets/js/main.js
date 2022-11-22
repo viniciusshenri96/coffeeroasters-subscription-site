66b5cba653f32761514f5f27bc243bf105d7cba1
Vinicius <vinicius_henrique96@hotmail.com>
2022-11-22 15:02:54 -0300

    :sparkles: feat: implementing summary text update

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,9 @@ const button = document.querySelector(".header__menu-ham");
 const nav = document.querySelector(".header__list");
 const selectionContainer = document.querySelector(".plan__selection");
 const orderSummaryContainer = document.querySelector(".plan__order-summary");
+const summaryContent = document.querySelector(".plan__order-summary--resume");
 
-const disabled = document.querySelector(".disabled");
-
+// Menu Mobile
 const openMenu = function () {
   // menu.classList.toggle("active");
   // close.classList.toggle("active");
@@ -17,18 +17,17 @@ const openMenu = function () {
 
 button.addEventListener("click", openMenu);
 
-// let html = `
-//     <p class="plan__order-summary--resume">
-//       “I drink my coffee using <span>Capsule</span>, with a
-//       <span>_____</span> type of bean. <span>_____</span> , sent to me
-//       <span>_____</span>.”
-//     </p>
-// `;
-
-// orderSummaryContainer.insertAdjacentHTML("beforeend", html);
-
+// Accordion selection option coffe
 selectionContainer.addEventListener("click", function (e) {
   const planPreferences = e.target.closest(".plan__preferences");
+
+  const disabled = document.querySelector(".disabled");
+  const titleDisabled = document
+    .getElementById("disabled")
+    .querySelector(".plan__title");
+  const contentDisabled = document
+    .getElementById("disabled")
+    .querySelector(".plan__content");
 
   const clickedTitleToggle = function (clickedTitle) {
     if (!clickedTitle) return;
@@ -45,20 +44,18 @@ selectionContainer.addEventListener("click", function (e) {
     const title = clickedReply.querySelector(".plan__reply--title");
 
     if (title.textContent === "Capsule") {
-      const titleDisabled = document
-        .getElementById("disabled")
-        .querySelector(".plan__title");
-      const contentDisabled = document
-        .getElementById("disabled")
-        .querySelector(".plan__content");
-
       if (titleDisabled.classList.contains("active")) {
         titleDisabled.classList.remove("active");
         contentDisabled.classList.remove("active");
       }
+      titleDisabled.style.opacity = "0.5";
+      summaryContent.innerHTML = `"I drink my coffee using<span data-order="1">_____</span>, with a <span data-order="2">_____</span> type of bean. <span data-order="3">_____</span>, sent to me <span data-order="4">_____</span>"`;
+
       return disabled.classList.add("block");
     }
+    titleDisabled.style.opacity = "1";
     disabled.classList.remove("block");
+    summaryContent.innerHTML = `"I drink my coffee as <span data-order="1">_____</span>, with a <span data-order="2">_____</span> type of bean. <span data-order="3">_____</span>, ground ala <span data-order="4">_____</span>, sent to me <span data-order="5">_____</span>"`;
   };
 
   if (planPreferences) {
