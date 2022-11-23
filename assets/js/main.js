c35fbc939983f6a5c67dad8da7b10deb99010d41
Vinicius <vinicius_henrique96@hotmail.com>
2022-11-23 20:57:31 -0300

    :bug: fix: implementing summary text update

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ const nav = document.querySelector(".header__list");
 const selectionContainer = document.querySelector(".plan__selection");
 const orderSummaryContainer = document.querySelector(".plan__order-summary");
 const summaryContent = document.querySelector(".plan__order-summary--resume");
+const blockGrind = document.querySelector("[data-disabled='block']");
 
 // Menu Mobile
 const openMenu = function () {
@@ -19,43 +20,109 @@ button.addEventListener("click", openMenu);
 
 // Accordion selection option coffe
 selectionContainer.addEventListener("click", function (e) {
-  const planPreferences = e.target.closest(".plan__preferences");
+  const classActive = "active";
 
-  const disabled = document.querySelector(".disabled");
-  const titleDisabled = document
-    .getElementById("disabled")
-    .querySelector(".plan__title");
-  const contentDisabled = document
-    .getElementById("disabled")
-    .querySelector(".plan__content");
+  const planPreferences = e.target.closest(".plan__preferences");
 
   const clickedTitleToggle = function (clickedTitle) {
     if (!clickedTitle) return;
 
-    clickedTitle.classList.toggle("active");
-    clickedTitle.nextElementSibling.classList.toggle("active");
+    clickedTitle.classList.toggle(classActive);
+    clickedTitle.nextElementSibling.classList.toggle(classActive);
   };
 
   const clickedReplyToggle = function (clickedReply) {
+    const titleDisabled = document
+      .getElementById("disabled")
+      .querySelector(".plan__title");
+    const contentDisabled = document
+      .getElementById("disabled")
+      .querySelector(".plan__content");
+
+    const disabledGrind = function (opa, pro) {
+      titleDisabled.style.opacity = opa;
+      return blockGrind.classList[`${pro}`]("block");
+    };
+
+    const textSummary = function (text) {
+      summaryContent.innerHTML = `${text}`;
+    };
+
     if (!clickedReply) return;
 
-    clickedReply.classList.add("active");
+    const titleSummary = clickedReply?.querySelector(".plan__reply--title");
+    if (!titleSummary) return;
 
-    const title = clickedReply.querySelector(".plan__reply--title");
+    clickedReply.classList.add(classActive);
 
-    if (title.textContent === "Capsule") {
-      if (titleDisabled.classList.contains("active")) {
-        titleDisabled.classList.remove("active");
-        contentDisabled.classList.remove("active");
-      }
-      titleDisabled.style.opacity = "0.5";
-      summaryContent.innerHTML = `"I drink my coffee using<span data-order="1">_____</span>, with a <span data-order="2">_____</span> type of bean. <span data-order="3">_____</span>, sent to me <span data-order="4">_____</span>"`;
+    const manipulation = clickedReply
+      .closest(".plan__preferences")
+      .querySelector(".js-manipulation");
+    const manipulationType = clickedReply
+      .closest(".plan__preferences")
+      .querySelector(".js-type");
+    const manipulationHow = clickedReply
+      .closest(".plan__preferences")
+      .querySelector(".js-how");
+    const manipulationGrind = clickedReply
+      .closest(".plan__preferences")
+      .querySelector(".js-grind");
+    const manipulationDeliver = clickedReply
+      .closest(".plan__preferences")
+      .querySelector(".js-deliver");
 
-      return disabled.classList.add("block");
+    let titleManipulationType,
+      titleManipulationHow,
+      titleManipulationGrind,
+      titleManipulationDeliver;
+
+    if (manipulationType) {
+      titleManipulationType = e.target
+        .closest(".plan__reply")
+        .querySelector(".plan__reply--title").textContent;
     }
-    titleDisabled.style.opacity = "1";
-    disabled.classList.remove("block");
-    summaryContent.innerHTML = `"I drink my coffee as <span data-order="1">_____</span>, with a <span data-order="2">_____</span> type of bean. <span data-order="3">_____</span>, ground ala <span data-order="4">_____</span>, sent to me <span data-order="5">_____</span>"`;
+
+    if (manipulationHow) {
+      titleManipulationHow = e.target
+        .closest(".plan__reply")
+        .querySelector(".plan__reply--title").textContent;
+    }
+
+    if (manipulationGrind) {
+      titleManipulationGrind = e.target
+        .closest(".plan__reply")
+        .querySelector(".plan__reply--title").textContent;
+    }
+
+    if (manipulationDeliver) {
+      titleManipulationDeliver = e.target
+        .closest(".plan__reply")
+        .querySelector(".plan__reply--title").textContent;
+    }
+
+    if (manipulation) {
+      const titleManipulation = e.target
+        .closest(".plan__reply")
+        .querySelector(".plan__reply--title").textContent;
+      console.log(titleManipulation);
+
+      if (titleManipulation === "Capsule") {
+        if (titleDisabled.classList.contains("active")) {
+          titleDisabled.classList.remove(classActive);
+          contentDisabled.classList.remove(classActive);
+        }
+        textSummary(
+          `"I drink my coffee using <span data-order="1">${titleManipulation}</span>, with a <span data-order="2">${titleManipulationType}</span> type of bean. <span data-order="3">${titleManipulationHow}</span>, sent to me <span data-order="4">${titleManipulationDeliver}</span>"`
+        );
+
+        return disabledGrind("0.5", "add");
+      }
+
+      disabledGrind("1", "remove");
+      textSummary(
+        `"I drink my coffee as <span data-order="1">${titleManipulation}</span>, with a <span data-order="2">_____</span> type of bean. <span data-order="3">$_____</span>, ground ala <span data-order="4">$_____</span>, sent to me <span data-order="5">$_____</span>"`
+      );
+    }
   };
 
   if (planPreferences) {
@@ -66,7 +133,7 @@ selectionContainer.addEventListener("click", function (e) {
 
     const planReply = planPreferences.querySelectorAll(".plan__reply");
 
-    planReply.forEach((i) => i.classList.remove("active"));
+    planReply.forEach((i) => i.classList.remove(classActive));
     clickedReplyToggle(clickedReply);
   }
 });
