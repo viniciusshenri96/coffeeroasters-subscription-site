1f336298875bcf5e581a6e028f0e810b10825a83
Vinicius <vinicius_henrique96@hotmail.com>
2022-11-26 18:55:14 -0300

    :bug: fix: solving bug implementing summary text update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,139 +1,124 @@
 "use strict";
 
 const button = document.querySelector(".header__menu-ham");
-// const menu = document.querySelector(".menu");
-// const close = document.querySelector(".close");
 const nav = document.querySelector(".header__list");
-const selectionContainer = document.querySelector(".plan__selection");
-const orderSummaryContainer = document.querySelector(".plan__order-summary");
-const summaryContent = document.querySelector(".plan__order-summary--resume");
-const blockGrind = document.querySelector("[data-disabled='block']");
-
+const containerSelection = document.querySelector(".plan__selection");
+const orderSummaryText = document.querySelector(".plan__order-summary--resume");
+const blockGrind = document.querySelector('[data-disabled="block"]');
+const checkedReply = document.querySelectorAll(".plan__reply");
+let activeButton = 0,
+  activeButtonTotal;
 // Menu Mobile
+
 const openMenu = function () {
-  // menu.classList.toggle("active");
-  // close.classList.toggle("active");
   nav.classList.toggle("active");
 };
 
 button.addEventListener("click", openMenu);
 
-// Accordion selection option coffe
-selectionContainer.addEventListener("click", function (e) {
-  const classActive = "active";
-
+containerSelection.addEventListener("click", function (e) {
   const planPreferences = e.target.closest(".plan__preferences");
 
-  const clickedTitleToggle = function (clickedTitle) {
-    if (!clickedTitle) return;
-
-    clickedTitle.classList.toggle(classActive);
-    clickedTitle.nextElementSibling.classList.toggle(classActive);
-  };
-
-  const clickedReplyToggle = function (clickedReply) {
-    const titleDisabled = document
-      .getElementById("disabled")
-      .querySelector(".plan__title");
-    const contentDisabled = document
-      .getElementById("disabled")
-      .querySelector(".plan__content");
-
-    const disabledGrind = function (opa, pro) {
-      titleDisabled.style.opacity = opa;
-      return blockGrind.classList[`${pro}`]("block");
-    };
-
-    const textSummary = function (text) {
-      summaryContent.innerHTML = `${text}`;
-    };
-
-    if (!clickedReply) return;
-
-    const titleSummary = clickedReply?.querySelector(".plan__reply--title");
-    if (!titleSummary) return;
-
-    clickedReply.classList.add(classActive);
-
-    const manipulation = clickedReply
-      .closest(".plan__preferences")
-      .querySelector(".js-manipulation");
-    const manipulationType = clickedReply
-      .closest(".plan__preferences")
-      .querySelector(".js-type");
-    const manipulationHow = clickedReply
-      .closest(".plan__preferences")
-      .querySelector(".js-how");
-    const manipulationGrind = clickedReply
-      .closest(".plan__preferences")
-      .querySelector(".js-grind");
-    const manipulationDeliver = clickedReply
-      .closest(".plan__preferences")
-      .querySelector(".js-deliver");
-
-    let titleManipulationType,
-      titleManipulationHow,
-      titleManipulationGrind,
-      titleManipulationDeliver;
-
-    if (manipulationType) {
-      titleManipulationType = e.target
-        .closest(".plan__reply")
-        .querySelector(".plan__reply--title").textContent;
-    }
-
-    if (manipulationHow) {
-      titleManipulationHow = e.target
-        .closest(".plan__reply")
-        .querySelector(".plan__reply--title").textContent;
-    }
-
-    if (manipulationGrind) {
-      titleManipulationGrind = e.target
-        .closest(".plan__reply")
-        .querySelector(".plan__reply--title").textContent;
-    }
-
-    if (manipulationDeliver) {
-      titleManipulationDeliver = e.target
-        .closest(".plan__reply")
-        .querySelector(".plan__reply--title").textContent;
-    }
-
-    if (manipulation) {
-      const titleManipulation = e.target
-        .closest(".plan__reply")
-        .querySelector(".plan__reply--title").textContent;
-      console.log(titleManipulation);
-
-      if (titleManipulation === "Capsule") {
-        if (titleDisabled.classList.contains("active")) {
-          titleDisabled.classList.remove(classActive);
-          contentDisabled.classList.remove(classActive);
-        }
-        textSummary(
-          `"I drink my coffee using <span data-order="1">${titleManipulation}</span>, with a <span data-order="2">${titleManipulationType}</span> type of bean. <span data-order="3">${titleManipulationHow}</span>, sent to me <span data-order="4">${titleManipulationDeliver}</span>"`
-        );
-
-        return disabledGrind("0.5", "add");
-      }
-
-      disabledGrind("1", "remove");
-      textSummary(
-        `"I drink my coffee as <span data-order="1">${titleManipulation}</span>, with a <span data-order="2">_____</span> type of bean. <span data-order="3">$_____</span>, ground ala <span data-order="4">$_____</span>, sent to me <span data-order="5">$_____</span>"`
-      );
-    }
-  };
-
   if (planPreferences) {
+    const containerDrink = e.target.closest(".js-drink"),
+      containerType = e.target.closest(".js-type"),
+      containerHow = e.target.closest(".js-how"),
+      containerGrind = e.target.closest(".js-grind"),
+      containerDeliver = e.target.closest(".js-deliver");
+    const classActive = "active";
+
+    // Grind Disable
+    const blockGrindFunction = function (pro, opa) {
+      blockGrind.classList[`${pro}`]("block");
+      blockGrind
+        .closest(".plan__preferences")
+        .querySelector(".plan__title").style.opacity = opa;
+    };
+
+    // Default grind
+    const defaultGrind = function () {
+      blockGrind
+        .closest(".plan__preferences")
+        .querySelector(".plan__content")
+        .classList.remove("active");
+      blockGrind
+        .closest(".plan__preferences")
+        .querySelector(".plan__title")
+        .classList.remove("active");
+
+      const replyDisabled = document
+        .getElementById("disabled")
+        .querySelectorAll(".plan__reply");
+
+      replyDisabled.forEach((dis) => dis.classList.remove("active"));
+      document.getElementById(`grind`).innerText = "_____";
+    };
+
+    const updateText = function (value) {
+      document.getElementById("grind").style.display = value;
+      document.getElementById("complet").style.display = value;
+    };
+
+    // Print title for order summary
+    const printTitle = function (containerName, nameData) {
+      if (containerName) {
+        const planReply = e.target.closest(".plan__reply");
+        if (!planReply) return;
+        const title = planReply.querySelector(".plan__reply--title").innerText;
+
+        document.getElementById(`${nameData}`).innerText = title;
+
+        if (title.includes("Filter") || title.includes("Espresso")) {
+          orderSummaryText.innerHTML = orderSummaryText.innerHTML.replace(
+            "using",
+            "as"
+          );
+          updateText("inline");
+          blockGrindFunction("remove", "1");
+        } else if (title === "Capsule") {
+          orderSummaryText.innerHTML = orderSummaryText.innerHTML.replace(
+            "as",
+            "using"
+          );
+          updateText("none");
+          blockGrindFunction("add", "0.5");
+          defaultGrind();
+        }
+
+        const checkReply = containerName.querySelectorAll(".plan__reply");
+        checkReply.forEach((check) => {
+          if (check.classList.contains("active")) {
+            activeButton++;
+            if (activeButton <= 3) {
+              console.log(activeButton);
+            }
+          }
+        });
+      }
+    };
+
+    if (containerType) printTitle(containerType, "type");
+    if (containerHow) printTitle(containerHow, "how");
+    if (containerGrind) printTitle(containerGrind, "grind");
+    if (containerDeliver) printTitle(containerDeliver, "deliver");
+    if (containerDrink) printTitle(containerDrink, "drink");
+
+    // Accordion
     const clickedTitle = e.target.closest(".plan__title");
-    clickedTitleToggle(clickedTitle);
+
+    if (clickedTitle) {
+      clickedTitle.classList.toggle(classActive);
+      clickedTitle.nextElementSibling.classList.toggle(classActive);
+    }
 
     const clickedReply = e.target.closest(".plan__reply");
 
-    const planReply = planPreferences.querySelectorAll(".plan__reply");
+    if (clickedReply) {
+      const planReply = planPreferences.querySelectorAll(".plan__reply");
+      planReply.forEach((i) => i.classList.remove(classActive));
+      clickedReply.classList.add(classActive);
 
-    planReply.forEach((i) => i.classList.remove(classActive));
-    clickedReplyToggle(clickedReply);
+      // const checkReply = planPreferences.querySelectorAll(".plan__reply");
+    }
   }
 });
