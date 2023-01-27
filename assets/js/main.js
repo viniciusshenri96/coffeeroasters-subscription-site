ff0275d9d8993b3ee76071109cc8e50ee95712fb
Vinicius <vinicius_henrique96@hotmail.com>
2023-01-27 19:57:32 -0300

    :heavy_plus_sign: build: installing dependencies, parcel, core-js and regenerator-runtime

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 // Garantindo que a maioria dos navegadores antigos suporta o aplicativo.
+import "core-js/stable";
+
+// polyfilling async/array
+import "regenerator-runtime/runtime";
 
 import planApp from "./modules/logic-plan.js";
 import animation from "./modules/animation.js";
